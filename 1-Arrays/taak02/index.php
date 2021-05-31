80c032608c22c7cf8b9d77f078b61298bfe06ca0
Nicolas <nicolas.gjdp@gmail.com>
2021-05-31 10:57:02 +0200

    Taak 2

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$persoon["voornaam"] = "Nicolas";
+$persoon["achternaam"] = "Gonsalves";
+$persoon["leeftijd"] = 17
+$persoon["opleiding"] = "Software Developer"
+
+echo "$persoon['voornaam'] $persoon['achternaam'] is $persoon['leeftijd'] jaar en doet de opleiding $persoon['opleiding'].";
+?>
+
+</body>
+</html>
